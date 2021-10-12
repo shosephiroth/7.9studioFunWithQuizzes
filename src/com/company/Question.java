7c56edc78f272ea4095601b594b858cca22ac6de
Mattx2k1 <44537080+Mattx2k1@users.noreply.github.com>
2021-10-11 20:10:10 -0400

    progress made, not complete

---
+++ src/com/company/Question.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public abstract class Question {
+    private String questionText;
+
+    private int pointValue;
+
+    public Question(String questionText, int pointValue) {
+        this.questionText = questionText;
+        this.pointValue = pointValue;
+    }
+
+    public int getPointValue() {
+        return pointValue;
+    }
+
+    public void displayQuestion() {
+        System.out.println(questionText);
+    }
+
+    public abstract void displayAnswer();
+
+    public abstract int getAnswers();
+
+    public void setPointValue(int pointValue) {
+        this.pointValue = pointValue;
+    }
+}
